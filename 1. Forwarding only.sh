2260f2c1906b0ee4e08c6a0d54838a9857cec963
milsnel <danilmirhazov@gmail.com>
2024-06-02 23:41:41 +0500

    SLON

--- 1. Forwarding only.sh
+++ 1. Forwarding only.sh
@@ -19,11 +19,11 @@ fi
 # Enable IPv4 and IPv6 forwarding
 SYSCTL_CONF="/etc/net/sysctl.conf"
 if [ -f "$SYSCTL_CONF" ]; then
-    sed -i 's/net.ipv4.ip_forward=0/net.ipv4.ip_forward=1/g' "$SYSCTL_CONF"
-    if grep -q 'net.ipv4.ip_forward=1' "$SYSCTL_CONF"; then
-        sed -i '/net.ipv4.ip_forward=1/a net.ipv6.conf.all.forwarding=1' "$SYSCTL_CONF"
+    sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward= 1 /g' "$SYSCTL_CONF"
+    if grep -q 'net.ipv4.ip_forward = 1' "$SYSCTL_CONF"; then
+        sed -i '/net.ipv4.ip_forward = 1/a net.ipv6.conf.all.forwarding = 1' "$SYSCTL_CONF"
     else
-        echo 'net.ipv6.conf.all.forwarding=1' >> "$SYSCTL_CONF"
+        echo 'net.ipv6.conf.all.forwarding = 1' >> "$SYSCTL_CONF"
     fi
 else
     error_exit "Файл $SYSCTL_CONF не найден."
