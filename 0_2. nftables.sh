ee15839de26a22589ce38dd63668eb5d8285eec9
milsnel <danilmirhazov@gmail.com>
2024-06-02 22:19:13 +0500

    Pegri Nedorasi

---
+++ 0_2. nftables.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+apt-get update && apt-get install -y nftables
+
+systemctl enable --now nftables
+
+nft add table ip nat
+
+nft add chain ip nat postrouting '{ type nat hook postrouting priority 0; }'
+
+nft add rule ip nat postrouting ip saddr 11.11.11.0/24 oifname "ens33" counter masquerade
+
+nft add rule ip nat postrouting ip saddr 22.22.22.0/24 oifname "ens33" counter masquerade
+
+nft add rule ip nat postrouting ip saddr 33.33.33.0/24 oifname "ens33" counter masquerade
+
+nft list ruleset | tail -n8 | tee -a /etc/nftables/nftables.nft
+
+systemctl restart nftables
+
+nft list ruleset
---
+++ 2. HQ-R.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e  # Прекращение работы скрипта при ошибке
+
+error_exit() {
+    echo "$1" 1>&2
+    exit 1
+}
+
+apt-get update && apt-get install -y frr
+
+# Define the path to the daemons file
+DAEMONS_FILE="/etc/frr/daemons"
+
+# Enable ospfd and ospf6d in the daemons file
+if [ -f "$DAEMONS_FILE" ]; then
+    sed -i 's/ospfd=no/ospfd=yes/g' "$DAEMONS_FILE"
+    sed -i 's/ospf6d=no/ospf6d=yes/g' "$DAEMONS_FILE"
+else
+    error_exit "Файл $DAEMONS_FILE не найден."
+fi
+
+# Enable and start the frr service
+systemctl enable --now frr || error_exit "Не удалось включить и запустить службу frr."
+
+# Configure OSPF and OSPF6 using vtysh
+vtysh <<EOF
+conf t
+router ospf
+passive-interface default
+network 192.168.100.0/26 area 0
+network 172.16.100.0/24 area 0
+exit
+
+interface GREtun
+no ip ospf network broadcast
+no ip ospf passive
+exit
+
+do wr mem
+
+router ospf6
+ospf6 router-id 11.11.11.2
+exit
+
+interface GREtun
+ipv6 ospf6 area 0
+exit
+
+interface ens34
+ipv6 ospf6 area 0
+exit
+
+do wr mem
+EOF
