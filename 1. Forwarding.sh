9b7aa6679ddd2ed443f23c57b676ea0fa4531b96
milsnel <danilmirhazov@gmail.com>
2024-06-05 16:51:19 +0500

    tipical

--- 1. Forwarding.sh
+++ 1. Forwarding.sh
@@ -7,19 +7,10 @@ error_exit() {
     exit 1
 }
 
-# Configure ens34 interface
-ENS34_OPTIONS="/etc/net/ifaces/ens34/options"
-if [ -f "$ENS34_OPTIONS" ]; then
-    sed -i 's/DISABLED=yes/DISABLED=no/g' "$ENS34_OPTIONS"
-else
-    error_exit "Файл $ENS34_OPTIONS не найден."
-fi
-
-
 # Enable IPv4 and IPv6 forwarding
 SYSCTL_CONF="/etc/net/sysctl.conf"
 if [ -f "$SYSCTL_CONF" ]; then
-    sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward= 1 /g' "$SYSCTL_CONF"
+    sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1 /g' "$SYSCTL_CONF"
     if grep -q 'net.ipv4.ip_forward = 1' "$SYSCTL_CONF"; then
         sed -i '/net.ipv4.ip_forward = 1/a net.ipv6.conf.all.forwarding = 1' "$SYSCTL_CONF"
     else
